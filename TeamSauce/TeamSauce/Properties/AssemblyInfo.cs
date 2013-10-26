b64026e9821d3fe71592dc3fab3a42248a1908af
Adam Witko <adam.witko@gmail.com>
2013-10-26 14:30:41 +0100

    Readding solution due to Josh

--- TeamSauce/TeamSauce/Properties/AssemblyInfo.cs
+++ TeamSauce/TeamSauce/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8de40f12-28dd-42d1-abc0-f1bf10c76912")]
+[assembly: Guid("80a68d45-d7df-493f-ade9-91eff9821d43")]
 
 // Version information for an assembly consists of the following four values:
 //
