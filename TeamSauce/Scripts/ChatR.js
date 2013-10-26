17e4c0f0ac73520c9ef7aa3481a961897d393269
Joshua Hawxwell <m@hawx.me>
2013-10-26 18:11:58 +0100

    First attempt at hooking up chat with signalR

--- TeamSauce/Scripts/ChatR.js
+++ TeamSauce/Scripts/ChatR.js
@@ -2,8 +2,11 @@
     var myConnection = $.connection("/teamchat");
 
     myConnection.received(function (data) {
-        alert('Received');
-        $("#messages").append("<li>" + data.Name + ': ' + data.Body + ': ' + data.Time + "</li>");
+        $('.chatbox').append('<li>' +
+                             '<span class="name">' + message.name + '</span>' +
+                             '<span class="body">' + message.body + '</span>' +
+                             '<time>' + moment(message.time).fromNow() + '</time>' +
+                             '</li>');
     });
 
     myConnection.error(function (error) {
@@ -13,10 +16,15 @@
     myConnection.start()
         .promise()
         .done(function () {
-            $("#send").click(function() {
-                var myName = $("#Name").val();
-                var myMessage = $("#Message").val();
-                myConnection.send(JSON.stringify({ name: myName, body: myMessage }));
+            $('.chat-message').keydown(function(event){
+                var keycode = (event.keyCode ? event.keyCode : event.which);
+
+                if (keycode == '13') {
+                    var body = $('.chat-message').val();
+
+                    myConnection.send(JSON.stringify({ name: currentUser, body: body }));
+                    event.preventDefault();
+                }
             });
         });
 });
