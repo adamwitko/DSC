dbd3cb7b3486f243e2497ceb84bbe6a4ab43f18c
Oliver Tomlinson <oliverjamestomlinson@gmail.com>
2013-10-26 15:39:59 +0100

    pls let this work

---
+++ TeamSauce/TeamSauce.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace TeamSauce
+{
+    public class TeamSauce : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
