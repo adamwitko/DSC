925928385bf5803068785a8cd81b1b1a4195263c
unknown <developer@OPERATI-K04E3DQ.esendex.com>
2013-10-27 03:54:00 +0000

    Moves inline javascript into its own file.

--- TeamSauce/Scripts/team-sauce-main.js
+++ TeamSauce/Scripts/team-sauce-main.js
@@ -1,25 +1,27 @@
 ﻿$(function() {
     var proxy = $.connection.teamSauceHub;
 
-    var init_LoginR = function() {
-        $("#log-in").click(function() {
+    var init_LoginR = function () {
+        $("#log-in").click(function () {
             var name = $('#username').val();
             var password = $('#password').val();
             proxy.server.logIn(name, password);
         });
     };
 
-    proxy.client.completed = function() {
+    proxy.client.completed = function (connectionId) {
+        alert(connectionId);
         init_SponsorChatR();
+        init_QuestionnaireR();
     };
 
-    proxy.client.loginFailed = function() {
-        alert('Username or password is incorrect');
+    proxy.client.loginFailed = function (connectionId) {
+        alert('Fail ' + connectionId);
     };
 
-    var init_SponsorChatR = function() {
+    var init_SponsorChatR = function () {
 
-        $("#send-sponsor-message").click(function() {
+        $("#send-sponsor-message").click(function () {
             var body = $('#sponsor-message').val();
             proxy.server.messageReceived(body);
         });
@@ -27,27 +29,26 @@
         proxy.server.getMessages();
     };
 
-    proxy.client.messagesLoaded = function(messages) {
+    proxy.client.messagesLoaded = function (messages) {
         alert('Messages Returned' + messages + ':' + messages.length);
         for (var idx = 0; idx < messages.length; idx++) {
             if (messages[idx].MessageType == 'Sponsor') {
                 $('#sponsor-feed').append('<li class="feed-sponsor">' +
-                    '<span class="name">' + messages[idx].Sender + '</span>' +
-                    '<span class="body">' + messages[idx].Message + '</span>' +
-                    '<time>' + messages[idx].Time + '</time>' +
-                    '</li>');
+                                 '<span class="name">' + messages[idx].Sender + '</span>' +
+                                 '<span class="body">' + messages[idx].Message + '</span>' +
+                                 '<time>' + messages[idx].Time + '</time>' +
+                                 '</li>');
             } else {
                 $('#sponsor-feed').append('<li>' +
-                    '<span class="name">' + messages[idx].Sender + '</span>' +
-                    '<span class="body">' + messages[idx].Message + '</span>' +
-                    '<time>' + messages[idx].Time + '</time>' +
-                    '</li>');
+                                '<span class="name">' + messages[idx].Sender + '</span>' +
+                                '<span class="body">' + messages[idx].Message + '</span>' +
+                                '<time>' + messages[idx].Time + '</time>' +
+                                '</li>');
             }
         }
     };
 
-
-    proxy.client.userMessage = function(message) {
+    proxy.client.userMessage = function (message) {
         $('#sponsor-feed').append('<li>' +
             '<span class="name">' + message.Sender + '</span>' +
             '<span class="body">' + message.Message + '</span>' +
@@ -55,7 +56,7 @@
             '</li>');
     };
 
-    proxy.client.sponsorMessage = function(message) {
+    proxy.client.sponsorMessage = function (message) {
         $('#sponsor-feed').append('<li class="feed-sponsor">' +
             '<span class="name">' + message.Sender + '</span>' +
             '<span class="body">' + message.Message + '</span>' +
@@ -63,7 +64,45 @@
             '</li>');
     };
 
-    $.connection.hub.start().done(function() {
+    var answers = [];
+    var questionnaireId;
+
+    var init_QuestionnaireR = function () {
+        $('#submitQuestionnaire').click(function () {
+            var questionnaireResponse = {
+                ratings: answers
+            };
+
+            proxy.server.complete(questionnaireId, JSON.stringify(questionnaireResponse));
+
+            answers = [];
+            questionnaireId = undefined;
+            $('#mymodal').modal('hide');
+        });
+    };
+
+    proxy.client.sentOutQuestionnaire = function (questionnaire) {
+        questionnaireId = questionnaire.id;
+
+        $.each(questionnaire.categoryQuestions, function (index, value) {
+            $('#questions').append('<div class="question" ng-repeat="question in questions">' +
+                '<span class="glyphicon glyphicon-record"></span><span>' + value.text +
+                '</span><div class="star" data-category=' + value.category + '"/></div>');
+        });
+
+        $('#mymodal').modal('show');
+
+        $('.star').raty({
+            'click': function (score) {
+                answers.push({
+                    category: $(this).attr('data-category'),
+                    value: score
+                });
+            }
+        });
+    };
+
+    $.connection.hub.start().done(function () {
         init_LoginR();
     });
 });
