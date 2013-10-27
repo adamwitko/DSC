e1835dcbb816807698b49e716041e266b686cd36
Joshua Hawxwell <m@hawx.me>
2013-10-27 05:15:01 +0000

    Make graph gracefully handle case where no data is returned

--- TeamSauce/Scripts/GraphR.js
+++ TeamSauce/Scripts/GraphR.js
@@ -27,6 +27,26 @@ $(function () {
     ];
 
     $.getJSON('/que/allaverage', function(data) {
+        var ctx = document.getElementById("that-big-graph").getContext("2d");
+
+        var opts = {
+            //Boolean - If we want to override with a hard coded scale
+            scaleOverride : true,
+
+            //** Required if scaleOverride is true **
+            //Number - The number of steps in a hard coded scale
+            scaleSteps : 10,
+            //Number - The value jump in the hard coded scale
+            scaleStepWidth : 1,
+            //Number - The scale starting value
+            scaleStartValue : 0
+        }
+
+        if (data.length < 1) {
+            new Chart(ctx).Line({labels: [], datasets: []}, opts);
+            return;
+        }
+
         var labels = [];
         for (var i = 0; i < data.length; i++) {
             labels[labels.length] = data[i].time;
@@ -58,21 +78,6 @@ $(function () {
         var data = {
             labels : labels,
             datasets : datasets
-        }
-
-        var ctx = document.getElementById("that-big-graph").getContext("2d");
-
-        var opts = {
-            //Boolean - If we want to override with a hard coded scale
-            scaleOverride : true,
-
-            //** Required if scaleOverride is true **
-            //Number - The number of steps in a hard coded scale
-            scaleSteps : 10,
-            //Number - The value jump in the hard coded scale
-            scaleStepWidth : 1,
-            //Number - The scale starting value
-            scaleStartValue : 0
         }
 
         new Chart(ctx).Line(data, opts);
