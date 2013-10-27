013ade88542d130d4c516e800be68ab2a1fa8600
unknown <developer@OPERATI-K04E3DQ.esendex.com>
2013-10-27 11:41:17 +0000

    Adds hub for simple team chat.

---
+++ TeamSauce/Hubs/TeamChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using TeamSauce.Models.Factories;
+using TeamSauce.Services;
+using TeamSauce.Services.Interfaces;
+
+namespace TeamSauce.Hubs
+{
+    [HubName("teamChatHub")]
+    public class TeamChatHub : Hub
+    {
+        private readonly IUserService _userService;
+        private readonly ITeamMessageModelFactory _modelFactory;
+
+        private readonly IHubConnectionContext _clientContext =
+            GlobalHost.ConnectionManager.GetHubContext<SponsorChatHub>().Clients;
+
+        public TeamChatHub()
+            : this(new UserService(), new TeamMessageModelFactory())
+        {
+        }
+
+        public TeamChatHub(IUserService userService, ITeamMessageModelFactory modelFactory)
+        {
+            _userService = userService;
+            _modelFactory = modelFactory;
+        }
+
+        public void MessageReceived(string message)
+        {
+            var sender = _userService.GetUser(Context.ConnectionId);
+            var model = _modelFactory.Create(message, sender.Name);
+
+            _clientContext.All.TeamMessageReceived(model);
+        }
+    }
+}
---
+++ TeamSauce/Models/Factories/TeamMessageModelFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamSauce.Models.Factories
+{
+    public class TeamMessageModelFactory : ITeamMessageModelFactory
+    {
+        public TeamMessageModel Create(string message, string username)
+        {
+            return new TeamMessageModel { Message = message, Sender = username, Time = DateTime.UtcNow};
+        }
+    }
+}
